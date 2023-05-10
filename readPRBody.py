b6c7c66e983b59a2390f2101758fc151198acc21
Kiran-Waghamare <106256550+Kiran-Waghamare@users.noreply.github.com>
2023-05-10 14:38:53 +0530

    Update readPRBody.py

--- readPRBody.py
+++ readPRBody.py
@@ -19,8 +19,7 @@ except FileNotFoundError:
     raise FileNotFoundError(f"File not found: {args.source}")
 
 # Define the regular expression pattern to match
-pattern = r"testsToBeRun\s*([\w, \n\r]*)"
-print(pattern)
+pattern = r"testsToBeRun\s*([\w, \n]*)"
 
 # Check if the file contents are empty
 if not filetext:
@@ -29,24 +28,24 @@ if not filetext:
 else:
     # Search for the pattern in the text
     new_s = re.search(pattern, filetext)
-    print(new_s)
 
     # If a match is found, print the contents of the capture group
     if new_s:
         test_names = new_s.group(1)
+        print("\n")
         print("Group 1 of Unit tests from PR Body: " + test_names)
         test_names = re.sub(r",\s*", ",", test_names)  # Remove existing spaces after commas
-        test_names = re.sub(r"[,\n\r]+", ", ", test_names)  # Replace commas and newlines with ", "
+        test_names = re.sub(r"[,\n]+", ", ", test_names)  # Replace commas and newlines with ", "
         test_names = re.sub(r", $", "", test_names)  # Remove trailing comma and space if present
 
-        print("Number of unit tests: " + test_names)
+        print("Number of unit tests: " + str(test_names.count(',') + 1))
 
         # Get environment file
         env_file = os.getenv('GITHUB_ENV')
 
         # Open and write test names into environment file
         with open(env_file, "w") as myfile:
-            myfile.write('PR_TESTS'+'='+test_names)
+            myfile.write('PR_TESTS'+'='+test_names+'\n')
 
         # Close the file
         f.close()
