fb399936369384ffa3066a3fecd4d5025a129fae
Kiran-Waghamare <106256550+Kiran-Waghamare@users.noreply.github.com>
2023-10-25 12:03:12 +0530

    add new script

--- pullRequest.py
+++ pullRequest.py
@@ -1,101 +1,74 @@
-from github import Github
+import json
+import requests
 from datetime import datetime
 
-# GitHub access token or username and password
-ACCESS_TOKEN = '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'
-REPO_NAME = 'Kiran-Waghamare/actions-pipelines'
 
-g = Github(ACCESS_TOKEN)
+# Constants
+URL_BASE = "https://api.github.com/repos/Kiran-Waghamare/actions-pipelines/pulls"
+HEADERS = {'Authorization': 'Bearer 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'}
+FILENAME = 'githubPR.json'
 
-repo = g.get_repo(REPO_NAME)
+def fetch_pull_requests(page):
+    """Fetches pull requests from the specified page."""
+    params = {
+        "state": "all",
+        "sort": "created",
+        "per_page": 100,
+        "page": page
+    }
+    response = requests.get(URL_BASE, headers=HEADERS, params=params)
+    response.raise_for_status()  # Raises an exception if the request fails
+    return response.json()
 
-# Function to calculate days since created/opened
-def days_since_created(created_at):
-    created_date = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
-    today = datetime.now()
-    return (today - created_date).days
+def fetch_all_pull_requests():
+    """Fetches all pull requests."""
+    pull_requests = []
+    for i in range(50):
+        data = fetch_pull_requests(i)
+        if data:
+            print(f"Data found on page {i}; analyzing...")
+            pull_requests.extend(data)
+        else:
+            break
+    return pull_requests
 
-# Function to filter and sort Pull Requests
-def filter_and_sort_pull_requests(state, time_filter, sort_by):
-    pull_requests = repo.get_pulls(state=state, sort='number', direction='asc')
+def filter_pull_requests(pull_requests, start_date, end_date):
+    """Filters pull requests that were merged between the start and end dates."""
+    # Convert dates to datetime objects
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+
     filtered_pull_requests = []
-    for pr in pull_requests:
-        created_date = pr.created_at.strftime("%Y-%m-%d")
-        if time_filter == 'day':
-            if created_date == datetime.now().strftime("%Y-%m-%d"):
-                filtered_pull_requests.append(pr)
-        elif time_filter == 'week':
-            if (datetime.now() - pr.created_at).days <= 7:
-                filtered_pull_requests.append(pr)
-        elif time_filter == 'month':
-            if (datetime.now() - pr.created_at).days <= 30:
-                filtered_pull_requests.append(pr)
-        elif time_filter == 'year':
-            if (datetime.now() - pr.created_at).days <= 365:
-                filtered_pull_requests.append(pr)
 
-    if sort_by == 'number':
-        filtered_pull_requests.sort(key=lambda x: x.number)
-    elif sort_by == 'created':
-        filtered_pull_requests.sort(key=lambda x: x.created_at)
+    for pr in pull_requests:
+        # Check if the pull request was merged
+        if pr["merged_at"]:
+            # Convert the merge date to a datetime object
+            merge_date = datetime.strptime(pr["merged_at"], "%Y-%m-%dT%H:%M:%SZ")
+
+            # Check if the merge date is within the specified range
+            if start_date <= merge_date <= end_date:
+                filtered_pull_requests.append(pr)
 
     return filtered_pull_requests
 
-# Generate HTML output
-html_output = """
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
-<body>
-    <h1>Pull Request Dashboard</h1>
-    <table>
-        <tr>
-            <th>PR Number</th>
-            <th>PR Title</th>
-            <th>PR Author</th>
-            <th>Created Date</th>
-            <th>Merged Date</th>
-            <th>Closed Date</th>
-            <th>Days Since Created</th>
-        </tr>
-"""
+def write_to_file(data, filename):
+    """Writes data to a file in JSON format."""
+    with open(filename, 'w') as f:
+        json.dump(data, f)
 
-# Example usage:
-filtered_pull_requests = filter_and_sort_pull_requests('open', 'day', 'number')
+def main():
+    """Main function of the script."""
+    pull_requests = fetch_all_pull_requests()
+    write_to_file(pull_requests, FILENAME)
+    print("All PRs have been fetched.")
+    
+    # ------------ Uncomment to filter by date ------------
+    # start_date = "2023-01-01"
+    # end_date = "2023-12-31"
+    # filtered_pull_requests = filter_pull_requests(pull_requests, start_date, end_date)
+    # print(filtered_pull_requests)
 
-for pr in filtered_pull_requests:
-    html_output += f"""
-        <tr>
-            <td>{pr.number}</td>
-            <td>{pr.title}</td>
-            <td>{pr.user.login}</td>
-            <td>{pr.created_at}</td>
-            <td>{pr.merged_at if pr.merged_at else 'Not Merged'}</td>
-            <td>{pr.closed_at if pr.closed_at else 'Not Closed'}</td>
-            <td>{days_since_created(pr.created_at)}</td>
-        </tr>
-    """
-
-html_output += """
-    </table>
-</body>
-</html>
-"""
-
-with open('PullRequestDashboard.html', 'w') as file:
-    file.write(html_output)
+# Runs the main function if the script is run directly
+if __name__ == "__main__":
+    main()
